c2745976cab65ee29b890cdffbf1d40f8e029c13
dakachi <david87dang@gmail.com>
2017-09-28 23:57:37 +0700

    finish taxdis

--- abc.php
+++ abc.php
@@ -1,41 +1,74 @@
 <?php
-require('fpdf.php');
+require 'fpdf.php';
+
+$order = wc_get_order( $_GET['order_id'] );
+
+$billing = $order->get_address('billing');
+
+$first_name = $billing['first_name'];
+$last_name = $billing['last_name'];
+$address = $billing['address_1'];
+$city = $billing['city'];
+$post_code = $billing['postcode'];
+
+$product_id = $_GET['product_id'];
+$url = get_the_post_thumbnail_url($product_id, 'full');
 
 class PDF extends FPDF
 {
 // Page header
-function Header()
-{
-    // Logo
-    $this->Image('logo.png',10,6,30);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Move to the right
-    $this->Cell(80);
-    // Title
-    $this->Cell(30,10,'Title',1,0,'C');
-    // Line break
-    $this->Ln(20);
-}
+    public function Header()
+    {
+        // Logo
+
+        // Arial bold 15
+        $this->SetFont('Arial', 'B', 15);
+
+        // Title
+        // Line break
+        $this->Ln(15);
+    }
 
 // Page footer
-function Footer()
-{
-    // Position at 1.5 cm from bottom
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Page number
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-}
+    public function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, 'Page ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+    }
 }
 
 // Instanciation of inherited class
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+$pdf->SetFont('Times', '', 12);
+
+$pdf->Image($url, 120, 15, 80);
+$pdf->SetFont('Arial', '', 18);
+$pdf->Text( 138, 47, '10 - MAR - 2018');
+
+$pdf->SetFont('Arial', '', 22);
+$pdf->Text( 140, 64, 'W358 RRE');
+
+$pdf->SetFont('Arial', '', 10);
+$pdf->Text( 147, 82, '10 - MAR - 2018');
+
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(0, 6, 'Mr. '.$first_name.' '. $last_name . ',', 0, 1);
+$pdf->Cell(0, 6, $address, 0, 1);
+$pdf->Cell(0, 6, $city, 0, 1);
+$pdf->Cell(0, 6, $post_code, 0, 1);
+
+$pdf->ln(5);
+
+$pdf->Cell(0, 10, 'Simply peel off the sticker and place inside your vehicle. ', 0, 1);
+$pdf->Cell(0, 10, 'Thank you for using Tax Disk Remind-Us.', 0, 1);
+
 $pdf->Output();
-?>
+
+
+exit;
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: PDF Generator for print
+Author: Wordpress
+*/
+
+add_action( 'admin_init', 'fpdf_load_fpdf' );
+function fpdf_load_fpdf() {
+	if($_GET['_wp_nonce'] && wp_verify_nonce( $_GET['_wp_nonce'], 'create_pdf' )) {
+		require_once dirname(__FILE__)  . '/abc.php';
+	}
+}
+
+
+function fpdf_add_pdf_meta_boxes()
+{
+    add_meta_box('pdf_printer', __('PDF Printer'), 'fpdf_printer_button', 'shop_order', 'side', 'high');
+}
+add_action('add_meta_boxes', 'fpdf_add_pdf_meta_boxes');
+
+function fpdf_printer_button() {
+	global $post;
+	$order = wc_get_order($post->ID);
+	$items = $order->get_items();
+	$item = array_pop($items);
+	$product_id = $item['product_id'];
+
+?>
+	<a target="_blank" href="<?php print wp_nonce_url(admin_url('?product_id='.$product_id.'&order_id=' . $post->ID), 'create_pdf', '_wp_nonce');?>"
+        class="button button-primary"><?php esc_html_e('Print', 'my-plugin-textdomain');?></a>
+<?php
+}
